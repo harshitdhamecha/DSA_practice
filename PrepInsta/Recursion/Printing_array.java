681c3862decff047df45635bbaf30309e38c4d5d
harshitdhamecha <137476624+harshitdhamecha@users.noreply.github.com>
2023-12-19 23:20:16 +0530

    Create Printing_array.java

---
+++ PrepInsta/Recursion/Printing_array.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+// printing an array using recursion
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int size = arr.length;
+        printarray(arr,size,0);
+
+    }
+
+
+    public static void printarray(int[] k, int size, int i) {
+
+        if(i==size) {
+            return;
+        }
+        System.out.print(k[i] + " ");
+        printarray(k,size,++i);
+    }
+}
